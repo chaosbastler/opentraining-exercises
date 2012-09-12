1fd9c44855e093bdce1e8668c0a49b7647862a07
Christian Skubich <christian.skubich@googlemail.com>
2012-09-12 16:01:02 +0200

    Improved shell script for converting .svg to .png
    Now only images that have not been converted yet will be processed.

--- svg/convert.sh
+++ svg/convert.sh
@@ -2,19 +2,28 @@
 
 echo "Starting image conversion."
 
+counter=0
 
 for f in *.svg 
 do	
 	NEWFILE=`basename $f .svg`.png
-	echo -e "\033[46;1;31m STARTING $NEWFILE \033[0m"
-
-	inkscape --without-gui --export-png=../$NEWFILE --export-height=600 $f
-	echo finished
-	echo ""
-
 	
-	echo -e "\033[46;1;31m FINISHED $NEWFILE \033[0m"
-	echo ""
+	# only convert if file does not exist
+	if [ -f "../$NEWFILE" ];	then
+		echo $NEWFILE "existiert bereits"
+		echo ""
+	else	
+		echo -e "\033[46;1;31m STARTING $NEWFILE \033[0m"
+
+		inkscape --without-gui --export-png=../$NEWFILE --export-height=600 $f
+		echo finished
+		echo ""
+
+		counter=`expr $counter + 1`
+		
+		echo -e "\033[46;1;31m FINISHED $NEWFILE \033[0m"
+		echo ""
+	fi	
 done
 
-echo "all images finished"
+echo "all images finished, converted $counter images"
